a88c94257e7e8e61eb8190e5e139344a97e785ab
jacob chudnovsky <jacob4seinfeld@gmail.com>
2018-07-08 15:27:54 -0700

    finish

---
+++ scrape.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup #extract the html from the request
+from selenium import webdriver #deal with the dynamic javascript
+
+###VARIABLES TO CHANGE####
+
+#URL of the specific product
+URL = "https://www.costco.com/Westport-Beautyrest-Fabric-Sleeper-Loveseat.product.100325179.html"
+
+#Path to the driver
+PATH_TO_DRIVER = '/Users/jacobchudnovsky/Downloads/chromedriver'
+
+##########################
+
+#Establish the driver
+driver = webdriver.Chrome(PATH_TO_DRIVER)
+
+#Get the contents of the URL
+driver.get(URL)
+
+#returns the inner HTML as a string
+innerHTML = driver.execute_script("return document.body.innerHTML")
+
+#closes the driver
+driver.close()
+
+#turns the html into an object to use with BeautifulSoup library
+soup = BeautifulSoup(innerHTML)
+
+## Now need to get the following from the page:
+#    1. seo meta tags
+#    2. product name
+#    3. product description
+#    4. category
+#    5. price
+#    6. embedded images
+
+'''
+def get_meta_tags():
+    ##
+
+def get_product_name():
+    ##
+
+def get_product_description():
+    ##
+
+def get_category():
+    ##
+
+def get_price():
+    ##
+
+def get_embedded_images():
+    ##
+'''
